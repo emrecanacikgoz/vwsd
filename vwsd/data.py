ec586bfde4a6135211361e0559467b11b37117ce
Ilker Kesen <ilkerksn@gmail.com>
2022-12-12 01:02:33 +0300

    model wip

--- vwsd/data.py
+++ vwsd/data.py
@@ -2,11 +2,13 @@ import os
 import os.path as osp
 import json
 import re
+from functools import wraps
 
+import click
 from tqdm import tqdm
 from PIL import Image
 import torch
-from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader
 from transformers import AutoTokenizer
 import pytorch_lightning as pl
 
@@ -17,11 +19,11 @@ ROOT = osp.abspath(osp.join(osp.dirname(osp.abspath(__file__)), '..'))
 
 
 class VWSDDataset(Dataset):
-    def __init__(self, root, split, processor=None, tokenizer=None, **kwargs):
+    def __init__(self, root, split, transform=None, tokenizer=None, **kwargs):
         super().__init__()
         self.root = process_path(root)
         self.split = split
-        self.transform = processor
+        self.transform = transform
         self.tokenizer = tokenizer
         self._load_data()
     
@@ -61,23 +63,135 @@ class VWSDDataset(Dataset):
         label = item['images'].index(item['gold'])
 
         if self.transform is not None:
-            images = [self.transform(image) for image in self.images]
+            images = [self.transform(x, return_tensors='pt') for x in images]
+            images = [x['pixel_values'] for x in images]
+            images = torch.cat(images, dim=0).unsqueeze(0)
 
-        word = context = prompt = None
+        word_ids = context_ids = prompt_ids = None
+        word_mask = context_mask = prompt_mask = None
         if self.tokenizer is not None:
             word = self._tokenize(item['word'])
+            word_ids = word['input_ids']
+            word_mask = word['attention_mask']
             context = self._tokenize(item['context'])
+            context_ids = context['input_ids']
+            context_mask = context['attention_mask']
             prompt = self._tokenize(item['word'] + ' ' + item['context'])
+            prompt_ids = prompt['input_ids']
+            prompt_mask = prompt['attention_mask']
 
         return {
+            'index': index,
             'raw_word': item['word'],
             'raw_context': item['context'],
             'image_files': item['images'],
             'gold_image': item['gold'],
-            'images': images,
-            'tokenized_word': word,
-            'tokenized_context': context,
-            'tokenized_prompt': prompt,
+            'pixel_values': images,
+            'word_ids': word_ids,
+            'word_mask': word_mask,
+            'context_ids': context_ids,
+            'context_mask': context_mask,
+            'prompt_ids': prompt_ids,
+            'prompt_mask': prompt_mask,
             'label': label,
         }
 
+
+class VWSDDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        train_dir=None,
+        trial_dir=None,
+        batch_size=8,
+        num_workers=0,
+        transform=None,
+        tokenizer=None,
+    ):
+        super().__init__()
+        self.train_dir = self.trial_dir = None
+        if train_dir is not None:
+            self.train_dir = process_path(train_dir)
+        if trial_dir is not None:
+            self.trial_dir = process_path(trial_dir)
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.transform = transform
+        self.tokenizer = tokenizer
+
+    @property
+    def pad_token_id(self):
+        pad_token_id = 0
+        if self.tokenizer is not None:
+            pad_token_id = self.tokenizer.pad_token_id
+        return pad_token_id
+
+    def setup(self, stage='fit'):
+        if stage == 'fit':
+            raise NotImplementedError('Training phase is not implemented yet.')
+        
+        if stage == 'predict' or stage == 'test':
+            self.trial_data = self.load_split(split='trial')
+    
+    def load_split(self, split):
+        if split == 'train':
+            root = self.train_dir
+        elif split == 'trial':
+            root = self.trial_dir
+
+        return VWSDDataset(
+            root=root,
+            split=split,
+            transform=self.transform,
+            tokenizer=self.tokenizer,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.trial_data,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=False,
+            collate_fn=clip_collate_fn(self.pad_token_id),
+        )
+
+    def predict_dataloader(self):
+        return DataLoader(
+            self.trial_data,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=False,
+            collate_fn=clip_collate_fn(self.pad_token_id),
+        )
+
+
+def clip_collate_fn(pad_token_id):
+    def _collate_fn(batch):
+        _helper = lambda key: [x[key] for x in batch]
+        B = len(batch)
+        T = max([x.numel() for x in _helper('prompt_ids')])
+        input_ids = torch.empty((B, T), dtype=torch.long)
+        input_ids.fill_(pad_token_id)
+        attention_mask = torch.empty((B, T), dtype=torch.long)
+        attention_mask.fill_(0)
+
+        for i, item in enumerate(batch):
+            L = item['prompt_ids'].numel()
+            input_ids[i, :L] = item['prompt_ids']
+            attention_mask[i, :L] = item['prompt_mask']
+        
+        pixel_values = torch.cat(_helper('pixel_values'), dim=0)
+        labels = torch.tensor(_helper('label')).view(1, -1)
+
+        return {
+            'pixel_values': pixel_values,
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'labels': labels,
+            'indexes': _helper('index'),
+            'raw_words': _helper('raw_word'),
+            'raw_contexts': _helper('raw_context'),
+        }
+
+    return _collate_fn
--- vwsd/model.py
+++ vwsd/model.py
@@ -4,15 +4,26 @@ from transformers import CLIPModel
 
 
 class CLIPZeroShotBaseline(nn.Module):
-    def __init__(self, config):
+    def __init__(self, architecture='openai/clip-vit-large-patch14'):
         super().__init__()
-        self.config = config
-        self.model = CLIPModel.from_pretrained(config.get('architecture'))
+        self.model = CLIPModel.from_pretrained(architecture)
 
-    def forward(self, images, text, *args, **kwargs):
-        B, N, C, H, W = images.shape[:2]
-        images = images.view(B*N, C, H, W)
-        image_features = self.model.get_image_features(images)
-        text_features = self.model.get_text_features(text)
-        raise NotImplementedError('WIP')
-        
+    def _expand(self, input, N):
+        B, T = input.shape
+        input = input.unsqueeze(1)
+        input = input.expand(B, N, T)
+        input = input.reshape(B*N, T)
+        return input
+
+    def forward(self, pixel_values, input_ids, attention_mask):
+        B, N, C, H, W = pixel_values.shape
+        pixel_values = pixel_values.reshape(B*N, C, H, W)
+        input_ids = self._expand(input_ids, N)
+        attention_mask = self._expand(attention_mask, N)
+        outputs = self.model(
+            pixel_values=pixel_values,
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+        )
+        logits_per_image = outputs.logits_per_image
+        return logits_per_image
